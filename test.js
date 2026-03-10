a529feceed6f7fbecf2ac55cde406b6bc44b6288
yasser <dev@yasser.email>
2026-03-10 05:30:33 -0300

    Move `abort()` and `timeout()` to `AbortSignal` (#1)

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 const test = require('brittle')
-const AbortController = require('.')
+const { AbortController, AbortSignal } = require('.')
+
+function noop() {}
 
 test('basic', (t) => {
-  t.plan(2)
+  t.plan(6)
 
   const controller = new AbortController()
 
@@ -13,5 +15,36 @@ test('basic', (t) => {
     t.is(event.target, signal)
   })
 
+  t.is(signal.aborted, false)
+
   controller.abort(new Error('Abort reason'))
+
+  t.is(signal.aborted, true)
+  t.is(signal.reason.message, 'Abort reason')
+  t.exception(() => signal.throwIfAborted(), /Abort reason/)
+})
+
+test('AbortSignal, abort', (t) => {
+  t.plan(1)
+
+  const signal = AbortSignal.abort(new Error('boom!'))
+
+  signal.addEventListener('abort', (event) => {
+    t.fail()
+  })
+
+  t.is(signal.reason.message, 'boom!')
+})
+
+test('AbortSignal, timeout', (t) => {
+  t.plan(1)
+
+  const keepTestAlive = setInterval(noop)
+  t.teardown(() => clearInterval(keepTestAlive))
+
+  const signal = AbortSignal.timeout(100)
+
+  signal.addEventListener('abort', (event) => {
+    t.is(signal.reason.message, 'The operation timed out')
+  })
 })
